e9f55665acfb1f02ecc7f26d659bf2a9ff2e0158
Ascaliko <ascaliko7@gmail.com>
2024-04-17 10:07:59 +0700

    init

---
+++ app/Models/Bank.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the IndoBank package.
+ *
+ * (c) PPDB SYSTEM <- | ascaliko7@gmail.com>
+ *
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Bank Model.
+ */
+class Bank extends Model
+{
+    /**
+     * Table name.
+     *
+     * @var string
+     */
+    protected $table = 'banks';
+}
